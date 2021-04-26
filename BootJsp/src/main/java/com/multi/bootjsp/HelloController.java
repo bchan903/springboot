828200eae5c128b2c8505fbff35e604f9f46f87e
bchan <teras99@hotmail.com>
2021-04-26 15:49:39 +0900

    Initial Loading

---
+++ BootJsp/src/main/java/com/multi/bootjsp/HelloController.java
@@ -0,0 +1,23 @@
+package com.multi.bootjsp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	@ResponseBody
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello BootJSP";
+				
+	}
+	
+	@RequestMapping("/hello")
+	public  String helloMsg(Model model) {
+		model.addAttribute("message", "안녕하세요");
+		return "hello";
+	}
+
+}
---
+++ BootJsp/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Insert title here</title>
+	</head>
+	<body>
+		hello.jsp 페이지 입니다.<br>
+		${message}<br>
+		<img src="/image/logo.png">
+	</body>
+</html>
